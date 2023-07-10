c533f625d3ddca3cb8883dd4176df35f429c4725
Azriel <azrihell.l@gmail.com>
2023-07-10 17:18:29 -0500

    first commit

---
+++ src/canvas/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Canvas = () => {
+  return (
+    <div>Canvas</div>
+  )
+}
+
---
+++ src/pages/Customizer.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Customizer = () => {
+  return (
+    <div>Customizer</div>
+  )
+}
+
