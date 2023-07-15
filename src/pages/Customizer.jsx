a0f11e2942c9b671e6725c6f2cc0c06343a1f6c3
Azriel <azrihell.l@gmail.com>
2023-07-15 13:04:48 -0500

    show active tab

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -25,7 +25,16 @@ export const Customizer = () => {
 
   // show tab content depending on the activeTab
   const generateTabContent = () => {
-
+    switch (activeEditorTab) {
+      case 'colorpicker':
+        return <ColorPicker />
+      case 'filepicker':
+        return <FilePicker />
+      case 'aipicker':
+        return <AiPicker />
+      default:
+        return null
+    }
   }
 
   return (
@@ -36,35 +45,37 @@ export const Customizer = () => {
             <div className='flex items-center min-h-screen'>
               <div className='editortabs-container tabs'>
                 {EditorTabs.map((tab) => (
-                  <Tab 
-                  key={tab.name} 
-                  tab={tab} 
-                  handleClick={() => {}}
+                  <Tab
+                    key={tab.name}
+                    tab={tab}
+                    handleClick={() => setActiveEditorTab(tab.name)}
                   />
                 ))}
+
+                {generateTabContent()}
               </div>
             </div>
           </motion.div>
 
           <motion.div className='absolute z-10 top-5 right-5' {...fadeAnimation}>
-            <CustomButton 
-            type='filled'
-            title='Go Back'
-            handleClick={() => state.intro = true}
-            customStyles='w-fit px-4 py-2.5 font-bold text-sm'
+            <CustomButton
+              type='filled'
+              title='Go Back'
+              handleClick={() => state.intro = true}
+              customStyles='w-fit px-4 py-2.5 font-bold text-sm'
             />
           </motion.div>
 
           <motion.div className='filtertabs-container' {...slideAnimation('up')}>
-          {FilterTabs.map((tab) => (
-                  <Tab 
-                  key={tab.name} 
-                  tab={tab} 
-                  isFilterTab
-                  isActiveTab=''
-                  handleClick={() => {}}
-                  />
-                ))}
+            {FilterTabs.map((tab) => (
+              <Tab
+                key={tab.name}
+                tab={tab}
+                isFilterTab
+                isActiveTab=''
+                handleClick={() => { }}
+              />
+            ))}
           </motion.div>
         </>
       )}
