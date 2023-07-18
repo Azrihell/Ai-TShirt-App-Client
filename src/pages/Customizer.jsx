7272148c3ddd5dfecedb58f2ee1faec688dfdfe8
Azriel <azrihell.l@gmail.com>
2023-07-18 16:48:58 -0500

    ai picker working

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -50,7 +50,22 @@ export const Customizer = () => {
     if (!prompt) return alert('Please enter a prompt')
 
     try {
-      // call on backend to generate ai image
+      setGeneratingImg(true)
+
+      const response = await fetch('http://localhost:8080/api/v1/dalle', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          prompt
+        })
+      })
+
+      const data = await response.json()
+
+      handleDecals(type, `data:image/png;base64,${data.photo}`)
+
     } catch (error) {
       alert(error)
     } finally {
